3bcd60aa4c4049be2332106e7163a6d644890d96
Victor Zacharia <victorzacharia110@email.com>
2026-08-01 05:11:16 +0300

    Fix missing Request import in CategoryController

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
